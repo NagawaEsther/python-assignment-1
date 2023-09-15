9a923fe40d9ca5b68457ad6c29e9e4b7dfe38704
NagawaEsther <142291818+NagawaEsther@users.noreply.github.com>
2023-09-15 21:20:20 +0300

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,58 @@
+# Even numbers > 8
+n=2,4,6
+sumofn=(2+4+6)
+print(sumofn)
+print("sum of even numbers less than 8" +str(sumofn))
+
+# Average of random numbers from keyboard
+Victoria=(7,9,5,3,1)
+sumvictoria=(7+9+5+3+1)
+#print(sumVictoria)
+Average=(sum(Victoria)/5)
+print(Average)
+print("Average"+ str(Average))
+
+
+#Operators
+a=-5+8*6
+y=-5
+z=8
+x=6
+n=z*x
+print(y+n)
+
+b=(55+9) % 9
+w=55
+p=9
+e=(w+p)
+print(e%p)
+
+c=20+-3*5/8
+i=20
+v=-3
+m=5
+q=8
+g=(v*m)
+w=(g/q)
+print(i+w)
+
+d=5+15/3*2-8 % 3
+f=5
+h=15
+l=3
+s=2
+a=8
+o=3
+Viv=(f+h)
+Kat=(l*s)
+Jav=(a%o)
+print(Viv/Kat-Jav)
+
+
+
+
+
+
+
+
+
